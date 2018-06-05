c95c39e67be987529f54739a6b0f22741f9f4704
William Blondel <contact@williamblondel.fr>
2018-06-05 09:21:39 +0200

    Add a session destroy function in Session/Session

--- Session/Session.php
+++ Session/Session.php
@@ -78,4 +78,28 @@ class Session
     {
         unset($_SESSION[$key]);
     }
+
+    /**
+     * Destroy the Session and Cookies.
+     */
+    public function destroy()
+    {
+        $_SESSION = [];
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
+        self::$instance = null;
+    }
 }
