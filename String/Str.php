2581ed2474ab6e16ef6a12064ab120ff80696cf5
William Blondel <contact@williamblondel.fr>
2018-06-14 10:35:44 +0200

    Add function to convert timestamp to ... ago text

--- String/Str.php
+++ String/Str.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 namespace Core\String;
+use \DateTime;
 
 class Str
 {
@@ -12,5 +13,34 @@ class Str
     {
         $alphabet = "0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
         return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
+    }
+
+    public static function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . '' : 'just now';
     }
 }
