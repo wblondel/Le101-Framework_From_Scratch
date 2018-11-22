93375f62f6ca23974a2691e9ca33361b16fe79e7
William Blondel <contact@williamblondel.fr>
2018-11-22 18:15:28 +0100

    fix return values for validators

--- Validator/Validator.php
+++ Validator/Validator.php
@@ -39,12 +39,15 @@ class Validator
     /**
      * @param string $field
      * @param string $errorMsg
+     * @return bool
      */
     public function isAlphaNum(string $field, string $errorMsg)
     {
         if (!preg_match('/^[a-zA-Z0-9_]+$/', $this->getField($field))) {
             $this->errors[$field] = $errorMsg;
+            return false;
         }
+        return true;
     }
 
 
@@ -53,36 +56,45 @@ class Validator
      * @param Database $database
      * @param string $table
      * @param string $errorMsg
+     * @return bool
      */
     public function isUnique(string $field, Database $database, string $table, string $errorMsg = '')
     {
         $record = $database->prepare("SELECT id FROM $table WHERE $field = ?", [$this->getField($field)], null, one);
         if ($record) {
             $this->errors[$field] = $errorMsg;
+            return false;
         }
+        return true;
     }
 
     /**
      * @param string $field
      * @param string $errorMsg
+     * @return bool
      */
     public function isEmail(string $field, string $errorMsg = '')
     {
         if (!filter_var($this->getField($field), FILTER_VALIDATE_EMAIL)) {
             $this->errors[$field] = $errorMsg;
+            return false;
         }
+        return true;
     }
 
     /**
      * @param $field
      * @param $errorMsg
+     * @return bool
      */
     public function isConfirmed(string $field, string $errorMsg = '')
     {
         $value = $this->getField($field);
         if (empty($value) || $value != $this->getField($field . '_confirm')) {
             $this->errors[$field] = $errorMsg;
+            return false;
         }
+        return true;
     }
 
     /**
